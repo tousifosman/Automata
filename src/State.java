b93a912e4b5909909c57eb949dcc178658259d00
tkelly910 <tkelly910@gmail.com>
2011-10-20 23:01:10 +0000

    Initial Commit, Interfaces

---
+++ src/State.java
@@ -0,0 +1,7 @@
+class State {
+    private boolean finalState;
+    
+    public boolean isFinal() {
+        return finalState;
+    }
+}
---
+++ src/interfaces/DFA.java
@@ -0,0 +1,11 @@
+
+package interfaces;
+
+
+public interface DFA extends FiniteAutomata {    
+    /**
+     * Returns the list of state resulting in moving from the fromState to the 
+     */
+    public State transition(State fromState, char letter);
+    
+}
