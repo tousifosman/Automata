e862020c3fb9ae583b7f2430dbffa51371dff5f1
tkelly910 <tkelly910@gmail.com>
2011-10-21 01:30:50 +0000

    packing

---
+++ src/automata/FiniteAutomata.java
@@ -0,0 +1,21 @@
+package automata;
+
+import java.util.List;
+
+public interface FiniteAutomata {
+    /**
+     * The start state of this FA
+     */
+    public State startState();
+    
+    /**
+     * Returns the list of all the states in the FA
+     */
+    public List<State> allStates();
+    
+    /**
+     * Returns the alphabet of the finite automata
+     */
+    public List<Character> alphabet();
+    
+}
