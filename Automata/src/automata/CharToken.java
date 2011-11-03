ae176f8c665d1ffa2c9225ca4cafd1f4dc9d1342
jwang302 <jwang302@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-11-03 02:56:50 +0000

    Saving

---
+++ Automata/src/automata/CharToken.java
@@ -0,0 +1,15 @@
+package automata;
+
+import java.util.Set;
+
+public class CharToken {
+	String name;
+	Set<Character> chars;
+	
+	public CharToken(String name, Set<Character> chars){
+		this.name= name;
+		
+		this. chars = chars;
+	
+	}
+}
