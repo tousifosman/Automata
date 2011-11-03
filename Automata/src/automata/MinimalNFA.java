02f97f8fb389fd466ff2b5694745541c0bbcc8d1
jwang302 <jwang302@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-11-03 02:24:16 +0000

    Min DFA Skeleton added

---
+++ Automata/src/automata/MinimalNFA.java
@@ -0,0 +1,71 @@
+package automata;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MinimalNFA implements NFA{
+
+	 private State startState;
+	 private Set<Character> alphabet;
+	 private Set<State> finalStates;
+	 //private HashMap<State, HashMap<Character, List<State>>> transitions;
+	
+	public MinimalNFA(){
+		this.alphabet = new HashSet<Character>();
+		this.finalStates = new HashSet<State>();
+		//this.transitions = new HashMap<State, HashMap<Character,List<State>>>();
+	}
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	
+	@Override
+	public Set<State> startStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<State> transitions(Set<State> fromStates, char letter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<State> transitions(State fromState, char letter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<State> allStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Character> alphabet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<State> finalStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public State startState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Automata/src/generateMinimalNFA/MinimalDFAGenerator.java
@@ -0,0 +1,5 @@
+package generateMinimalNFA;
+
+public class MinimalDFAGenerator {
+
+}
