b40e005567f6ff6565b5133ecb6ea92fbb630415
tkelly910 <tkelly910@gmail.com>
2011-11-03 17:30:35 +0000

    * ScannerDriver: Added method to create an NFA for testing

--- Automata/src/ScannerDriver.java
+++ Automata/src/ScannerDriver.java
@@ -1,8 +1,11 @@
 
 import automata.DFA;
 import automata.MapBasedDFA;
+import automata.MapBasedNFA;
+import automata.NFA;
 import automata.State;
 import automata.Token;
+import conversion.NFAtoDFA;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -32,9 +35,11 @@ public class ScannerDriver {
      * of the tokens
      */
     public static void main(String[] args) {
-        // TODO : Replace with generating DFA
-        DFA dfa = defaultDFA();
-
+        // TODO : Replace with generating NFA
+        NFA nfa = defaultNFA();
+        DFA dfa = NFAtoDFA.dfaFromNFA(nfa);
+        // TODO : DFA minimization
+        
         try {
             String fileName = "sample_code.txt";
             if (args.length > 0) {
@@ -242,5 +247,130 @@ public class ScannerDriver {
             dfa.addTransisition(upperLetterDigit, letter, upperLetterDigit);
         }
         return dfa;
+    }
+    
+    /**
+     * A temporary NFA used for testing.
+     */
+    private static NFA defaultNFA() {
+        State startState = new State();
+        MapBasedNFA nfa = new MapBasedNFA(startState);
+
+        Token start = new Token("SMALLCASE", true);
+        Token end = new Token("SMALLCASE", false);
+        Token overallStart = new Token("IDENTIFIER", true);
+
+        Stack<Token> stack = new Stack<Token>();
+        stack.push(overallStart);
+        stack.push(start);
+        stack.push(end);
+        State small = new State(stack);
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            nfa.addTransisition(startState, letter, small);
+        }
+        start = new Token("UPPERCASE", true);
+        end = new Token("UPPERCASE", false);
+        overallStart = new Token("CONSTANT", true);
+
+        stack = new Stack<Token>();
+        stack.push(overallStart);
+        stack.push(start);
+        stack.push(end);
+        State upper = new State(stack);
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            nfa.addTransisition(startState, letter, upper);
+        }
+        start = new Token("LETTER", true);
+        end = new Token("LETTER", false);
+
+        stack = new Stack<Token>();
+        stack.push(start);
+        stack.push(end);
+
+        State smallLetter = new State(stack);
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            nfa.addTransisition(small, letter, smallLetter);
+        }
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            nfa.addTransisition(small, letter, smallLetter);
+        }
+
+        start = new Token("LETTER", true);
+        end = new Token("LETTER", false);
+
+        stack = new Stack<Token>();
+        stack.push(start);
+        stack.push(end);
+
+        State upperLetter = new State(stack);
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            nfa.addTransisition(upper, letter, upperLetter);
+        }
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            nfa.addTransisition(upper, letter, upperLetter);
+        }
+
+
+        Token overallEnd = new Token("IDENTIFIER", false);
+        start = new Token("DIGIT", true);
+        end = new Token("DIGIT", false);
+
+        stack = new Stack<Token>();
+        stack.push(start);
+        stack.push(end);
+        stack.push(overallEnd);
+
+        State smallLetterDigit = new State(stack);
+        smallLetterDigit.setFinal(true);
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            nfa.addTransisition(smallLetter, letter, smallLetter);
+        }
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            nfa.addTransisition(smallLetter, letter, smallLetter);
+        }
+        for (char letter = '0'; letter <= '9'; letter++) {
+            nfa.addTransisition(smallLetter, letter, smallLetterDigit);
+        }
+        overallEnd = new Token("CONSTANT", false);
+        start = new Token("DIGIT", true);
+        end = new Token("DIGIT", false);
+
+        stack = new Stack<Token>();
+        stack.push(start);
+        stack.push(end);
+        stack.push(overallEnd);
+
+        State upperLetterDigit = new State(stack);
+        upperLetterDigit.setFinal(true);
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            nfa.addTransisition(upperLetter, letter, upperLetter);
+        }
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            nfa.addTransisition(upperLetter, letter, upperLetter);
+        }
+        for (char letter = '0'; letter <= '9'; letter++) {
+            nfa.addTransisition(upperLetter, letter, upperLetterDigit);
+        }
+
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            nfa.addTransisition(smallLetterDigit, letter, smallLetter);
+        }
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            nfa.addTransisition(smallLetterDigit, letter, smallLetter);
+        }
+        for (char letter = '0'; letter <= '9'; letter++) {
+            nfa.addTransisition(smallLetterDigit, letter, smallLetterDigit);
+        }
+
+        for (char letter = 'a'; letter <= 'z'; letter++) {
+            nfa.addTransisition(upperLetterDigit, letter, upperLetter);
+        }
+        for (char letter = 'A'; letter <= 'Z'; letter++) {
+            nfa.addTransisition(upperLetterDigit, letter, upperLetter);
+        }
+        for (char letter = '0'; letter <= '9'; letter++) {
+            nfa.addTransisition(upperLetterDigit, letter, upperLetterDigit);
+        }
+        return nfa;
     }
 }
