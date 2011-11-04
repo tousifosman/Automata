83b1dc941e7a9868d28045aba07b97e4c0a76394
jwang302 <jwang302@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-11-04 00:31:54 +0000

    initial import

---
+++ AutoTest/src/CharToken.java
@@ -0,0 +1,15 @@
+
+
+import java.util.Set;
+
+public class CharToken {
+	String name;
+	Set<Character> chars;
+	
+	public CharToken(String name, Set<Character> chars){
+		this.name= name;
+		
+		this. chars = chars;
+	
+	}
+}
---
+++ AutoTest/src/State.java
@@ -0,0 +1,59 @@
+
+
+import java.util.Random;
+import java.util.Stack;
+
+/**
+ * Tokens are stored in the state in a stack. The top token is
+ * the most general token applying to the State, and the bottom token is the 
+ * most specific/smallest token.
+ */
+public class State {
+    private boolean finalState;
+    private String name;
+    private int id;
+    private Stack<Token> tokens;
+    
+    private static int count = 0;
+    
+    public State() {
+        this(new Stack<Token>());
+    }
+    
+    public State(Stack<Token> tokens) {
+        this.tokens = new Stack<Token>();
+        this.tokens.addAll(tokens);
+        
+        this.name = "q" + count;
+        this.id = (new Random()).nextInt();
+        this.finalState = false;
+        
+        count++;
+    }
+    
+    public Stack<Token> getTokens() {
+        Stack<Token> stack = new Stack<Token>();
+        stack.addAll(this.tokens);
+        return stack;
+    }
+   
+    public boolean isFinal() {
+        return finalState;
+    }
+    
+    public void setFinal(boolean finalState) {
+        this.finalState = finalState;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        return (o instanceof State && ((State)o).id == this.id);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 41 * hash + this.id;
+        return hash;
+    }
+}
