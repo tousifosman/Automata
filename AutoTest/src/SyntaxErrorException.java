c96d35d3d0a1efb82efec9d65950f2bed85be939
jwang302 <jwang302@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-11-04 04:17:23 +0000

    Recursive Descent Parser

---
+++ AutoTest/src/SyntaxErrorException.java
@@ -0,0 +1,26 @@
+
+public class SyntaxErrorException extends Exception {
+	String mistake;
+	//----------------------------------------------
+	// Default constructor - initializes instance variable to unknown
+	  public SyntaxErrorException()
+	  {
+	    super();             // call superclass constructor
+	    mistake = "Syntac mismatch";
+	  }
+	  
+	//-----------------------------------------------
+	// Constructor receives some kind of message that is saved in an instance variable.
+	  public SyntaxErrorException(String err)
+	  {
+	    super(err);     // call super class constructor
+	    mistake = err;  // save message
+	  }
+	  
+	//------------------------------------------------  
+	// public method, callable by exception catcher. It returns the error message.
+	  public String getError()
+	  {
+	    return mistake;
+	  }
+}
