d5ca1c676b1ea38eccff0d5b2c547c7f80ee42f4
jwang302 <jwang302@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-11-04 00:31:54 +0000

    initial import

---
+++ AutoTest/src/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String fileName = "sample_input_specification.txt";
+		MinimalDFAGenerator mmdg = new MinimalDFAGenerator(fileName);
+
+	}
+
+}
---
+++ AutoTest/src/NFA.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Set;
+
+public interface NFA extends FiniteAutomata {    
+    /**
+     * Returns the start state, in addition to any states reachable from the start state after epislon transitions.
+     */
+    public Set<State> startStates();
+    
+    /**
+     * Returns the list of possible states that come out the given fromStates across a letter transition
+     */
+    public Set<State> transitions(Set<State> fromStates, char letter);
+    
+    /**
+     * Returns the list of possible states that come out the given fromState across a letter transition
+     */
+    public Set<State> transitions(State fromState, char letter);
+}
