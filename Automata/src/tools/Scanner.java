cdd73042769189789917de98ba913e3278aded82
paul264 <paul264@gmail.com>
2011-11-05 00:42:06 +0000

    Completed Scanner.

--- Automata/src/tools/Scanner.java
+++ Automata/src/tools/Scanner.java
@@ -1,12 +1,9 @@
 package tools;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 import automata.Token;
 import automata.CharToken;
 import automata.NFA;
+import exceptions.SyntaxErrorException;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -19,11 +16,12 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 /**
- *
+ * A lexer object which includes file operations, Character class parsing, and Token scanning. To be used from a parser wrapper.
  * @author Paul
  */
 public class Scanner {
 
+// <editor-fold defaultstate="collapsed" desc="Field Definitions">
     public static Map<String, CharToken> charClasses;
     public static Map<String, NFA> identifiers;
     //private Set<Token> tokenList = new HashSet<Token>();
@@ -31,9 +29,29 @@ public class Scanner {
     private FileReader file;
     private BufferedReader reader;
     private Token currentToken;
-    private Deque<Character> tokenBuffer = new LinkedList<Character>();
+
+    private class LLPlus extends LinkedList<Character> {
+
+        public String traverse() {
+            StringBuilder t = new StringBuilder();
+            for (Character u : this) {
+                t.append(u);
+            }
+            return t.reverse().toString();
+        }
+    }
+    private LLPlus tokenBuffer = new LLPlus();
     private Deque<Character> lookAhead = new LinkedList<Character>();
-    public boolean busy;
+    private boolean busy;
+// </editor-fold>
+
+    /**
+     * Indicates the busy state of the Scanner object.
+     * @return True, if busy.
+     */
+    public boolean isBusy() {
+        return this.busy;
+    }
 
     public Scanner(String filename) throws FileNotFoundException, IOException {
         this.busy = true;
@@ -72,10 +90,13 @@ public class Scanner {
 
             String charClassName = CNBuilder.toString();
 
-            while (chars[i++] != '[') {
-                if (i > chars.length) {
-                    syntaxError("Missing \'[\' in character class \'" + charClassName + "\'!");
+            try {
+                while (chars[i++] != '[') {
+                    if (i > chars.length) {
+                        throw new SyntaxErrorException("Missing '[' in character class '" + charClassName + "'!");
+                    }
                 }
+            } catch (SyntaxErrorException e) {
             }
 
             boolean opposite = false;
@@ -84,15 +105,12 @@ public class Scanner {
             for (i = i + 1; i < chars.length; i++) {
 
                 if (chars[i] == ']') {
-
                     break;
                 }
-
                 if (chars[i] == '^') {
                     opposite = true;
                 }
                 if (chars[i] == '\\') {
-                    //i++;
                     int newIndex = i + 1;
                     if (Constants.specialCharsList.contains(chars[newIndex])) {
                         charSet.add(chars[newIndex]);
@@ -163,17 +181,17 @@ public class Scanner {
             CharToken token = new CharToken(charClassName, charSet);
             charClasses.put(charClassName, token);
         }
-        if (!currentLine.startsWith("%")) {
-            syntaxError("Can't find Token definitions!");
+
+        try {
+            if (!currentLine.startsWith("%")) {
+                throw new SyntaxErrorException("Can't find Token definitions!");
+            }
+        } catch (SyntaxErrorException e) {
         }
+
     }
 
-    private void syntaxError(String message) {
-        System.out.println(message);
-        Runtime.getRuntime().halt(1);
-    }
-
-    private void scanToken() throws IOException {
+    private Token scanToken() throws IOException {
         if (lookAhead.isEmpty()) {
             // Starting a new line
             scanLine();
@@ -181,70 +199,70 @@ public class Scanner {
                 lookAhead.addLast(currentLine.toCharArray()[i]);
             }
             /* We are reading an identifier */
-            newIdentifier();
+            return newIdentifier();
         } else {
-            /* Starting at some random point */
-            if (pop() == null) {
-                if (tokenBuffer.isEmpty()) {
-                    currentToken = Constants.EPSILON;
-                    return;
-                } else {
-                    syntaxError("Unexpected end of line!");
+            try {
+                /* Starting at some random point */
+                // If lookAhead is empty, either detect an epsilon token or a syntax error
+                if (pop() == null) {
+                    if (tokenBuffer.isEmpty()) {
+                        return Constants.EPSILON;
+                    } else {
+                        throw new SyntaxErrorException("Unexpected end of line!");
+                    }
                 }
+                switch (tokenBuffer.peek()) {
+                    case '\\':
+                        /* Escaped character token */
+                        return new Token("\\".concat(pop().toString()));
+                    case '$':
+                        /* Character class */
+                        while (!Constants.specialCharsList.contains(pop())) {
+                        }
+                        unpop();
+                        return new Token(tokenBuffer.traverse());
+                    default:
+                        /* Single Character */
+                        return new Token(tokenBuffer.peek().toString());
+                }
+            } catch (SyntaxErrorException e) {
             }
-            switch (tokenBuffer.peekFirst()) {
-                case '|':
-                    currentToken = Constants.UNION;
-                    break;
-                case '(':
-                    currentToken = Constants.LPAREN;
-                    break;
-                case ')':
-                    currentToken = Constants.RPAREN;
-                    break;
-                case '*':
-                    currentToken = Constants.ASTERISK;
-                    break;
-                case '+':
-                    currentToken = Constants.PLUS;
-                    break;
-                case '.':
-                    currentToken = Constants.FULLSTOP;
-                    break;
-                case '^':
-                    currentToken = Constants.CARAT;
-                    break;
-                case '[':
-                    currentToken = Constants.LBRACKET;
-                    break;
-                case ']':
-                    currentToken = Constants.RBRACKET;
-                    break;
-                case '?':
-                    currentToken = Constants.QUESTION;
-                    break;
-            }
-            /*HANDLE BACKSLASH and DOLLAR SIGN*/
-            
-            //if (!tokenList.contains(currentToken)) tokenList.add(currentToken);
         }
+        return null;
     }
 
-    private void newIdentifier() {
-        if (pop() != '$') {
-            syntaxError("Missing \'$\' leading Identifier!");
+    private Token newIdentifier() {
+        try {
+            if (pop() != '$') {
+                throw new SyntaxErrorException("Missing '$' leading Identifier!");
+            }
+        } catch (SyntaxErrorException e) {
         }
-        while (lookAhead.pollFirst() != ' ' && pop() != '\t') {
+        while (lookAhead.pollFirst() != ' ' && popVerbatim() != '\t') {
         }
         /* Space marks the end of an identifier */
-        while (!tokenBuffer.isEmpty()) {
-            buffer.concat(tokenBuffer.pop().toString());
-        }
-        currentToken = new Token(buffer.trim(), false);
-        //if (!tokenList.contains(currentToken)) tokenList.add(currentToken);
+        return new Token(tokenBuffer.traverse());
     }
 
+    /**
+     * Pops the first character off the lookAhead and pushes it onto the token buffer. Ignores whitespace. To preserve whitespace, use popVerbatim().
+     * @return the popped character.
+     */
     private Character pop() {
+        Character t = popVerbatim();
+        if (java.lang.Character.isWhitespace(t)) {
+            tokenBuffer.poll();
+            return pop();
+        } else {
+            return t;
+        }
+    }
+
+    /**
+     * Pops the first character off the lookAhead and pushes it onto the token buffer. Preserve whitespace.
+     * @return the popped character.
+     */
+    private Character popVerbatim() {
         tokenBuffer.push(lookAhead.pollFirst());
         return tokenBuffer.peekFirst();
     }
@@ -254,11 +272,28 @@ public class Scanner {
     }
 
     public Token peekToken() throws IOException {
-        scanToken();
-        return currentToken;
+        return (currentToken = scanToken());
     }
 
-    public void matchToken(Token token) {
-        return;
+    /**
+     * Clears the token buffer if token matches.
+     * @param token The token to be compared against.
+     * @return True if tokens matched, false otherwise.
+     */
+    public boolean matchToken(Token token) {
+        if (currentToken.equals(token)) {
+            currentToken = new Token("");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Clears the token buffer if token matches.
+     * @param token The token to be compared against.
+     */
+    public void matchToken() throws IOException {
+        currentToken = currentToken.equals(new Token("")) || currentToken == null ? scanToken() : currentToken;
     }
 }
