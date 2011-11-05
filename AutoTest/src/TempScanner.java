9e4c57f220c07fd83a15a253ed58c134e64258a0
jwang302 <jwang302@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-11-05 09:02:15 +0000

    Debug not complete

--- AutoTest/src/TempScanner.java
+++ AutoTest/src/TempScanner.java
@@ -1,19 +1,58 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class TempScanner {
-
-	
-	
+	Queue<Token> queue;
+	 
 	
 	public TempScanner(){
-				
+		queue = new LinkedList<Token>();
+		//Token token = new Token("$SMALLCASE", false);
+		Token token = new Token("a", false);
+		queue.add(token);
+		//token = new Token("(", false);
+		token = new Token("b", false);
+		queue.add(token);
+		//token = new Token("$LETTER", false);
+		token = new Token("(", false);
+		queue.add(token);
+		//token = new Token("|",false);
+		token = new Token("c", false);
+		queue.add(token);
+		//token = new Token("$DIGIT", false);
+		token = new Token("|", false);
+		queue.add(token);
+		//token = new Token(")", false);
+		token = new Token("d", false);
+		queue.add(token);
+		//token= new Token("*", false);
+		token = new Token(")", false);
+		queue.add(token);
+		token = new Token("*", false);
+		queue.add(token);
+		
 	}
 	
 	public Token peek(){
-		return null;
+		Token token =queue.peek();
+		if(token ==null){
+			token = new Token("null", false);
+		}
+		//if(token.)
+		return token; 
 	}
 	
-	public boolean matchToken(Token token){
-		return false;
+	public boolean matchToken(Token token) throws SyntaxErrorException{
+		Token pollToken = queue.poll();
+		if(token.equals(pollToken)){
+			System.out.println("Matched Token: "+ pollToken.getValue());
+			return true;
+		}
+		else {
+			//System.out.println("Mis-matched token: "+ pollToken.getValue()+"\nTrying to match: "+token.getValue());
+			throw new SyntaxErrorException();
+		}
 	}
 	
 			
