5e82ec82b907df91b32754ff0ce36871bb558bd0
paul264@gmail.com <paul264@gmail.com@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-11-05 19:37:35 +0000

    Javadoc updates.

--- Automata/src/exceptions/SyntaxErrorException.java
+++ Automata/src/exceptions/SyntaxErrorException.java
@@ -1,30 +1,35 @@
 package exceptions;
 
-
-
-
+/**
+ * A general Exception to be called in indication of an input file failing to meet specifications.
+ * @author 
+ */
 public class SyntaxErrorException extends Exception {
-	String mistake;
-	//----------------------------------------------
-	// Default constructor - initializes instance variable to unknown
-	  public SyntaxErrorException()
-	  {
-	    super();             // call superclass constructor
-	    mistake = "Syntax mismatch";
-	  }
-	  
-	//-----------------------------------------------
-	// Constructor receives some kind of message that is saved in an instance variable.
-	  public SyntaxErrorException(String err)
-	  {
-	    super(err);     // call super class constructor
-	    mistake = err;  // save message
-	  }
-	  
-	//------------------------------------------------  
-	// public method, callable by exception catcher. It returns the error message.
-	  public String getError()
-	  {
-	    return mistake;
-	  }
+
+    String mistake;
+
+    /**
+     * Default constructor - initializes instance variable to unknown
+     */
+    public SyntaxErrorException() {
+        super();             // call superclass constructor
+        mistake = "Syntax mismatch";
+    }
+
+    /**
+     * Constructor receives some kind of message that is saved in an instance variable.
+     * @param err Error message.
+     */
+    public SyntaxErrorException(String err) {
+        super(err);     // call super class constructor
+        mistake = err;  // save message
+    }
+
+    /**
+     * Returns an error after being caught.
+     * @return The error as a String.
+     */
+    public String getError() {
+        return mistake;
+    }
 }
