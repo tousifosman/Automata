4b6be5bd92bac3d8d4a05ee295aee00c653d0548
tkelly910 <tkelly910@gmail.com>
2011-12-02 21:50:54 +0000

    Nodes

--- 3240_Phase2/src/ast/Node.java
+++ 3240_Phase2/src/ast/Node.java
@@ -1,8 +1,11 @@
 
 package ast;
 
+import java.util.List;
+
 public abstract class Node {
-    
-    
-    
+    public abstract Object getType();
+    public abstract Object getToken();
+    public abstract Node getNextNode();
+    public abstract List<Node> getSubNodes();
 }
--- 3240_Phase2/src/ast/StatementNode.java
+++ 3240_Phase2/src/ast/StatementNode.java
@@ -1,6 +1,28 @@
 
 package ast;
 
+import java.util.List;
+
 public class StatementNode extends Node{
+
+    @Override
+    public Object getType() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Object getToken() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Node getNextNode() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<Node> getSubNodes() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
     
 }
