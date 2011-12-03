c647c16f002f0535b06a6197b6c227163f8b1fe4
jwang302 <jwang302@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-12-03 04:42:50 +0000

    Hard Code table up to <statement>

--- 3240_Phase2/src/parser/LLParser.java
+++ 3240_Phase2/src/parser/LLParser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,7 @@ import java.util.Stack;
 
 public class LLParser {
 	
-	Map<RULE_TYPE, Map<TOKEN_TYPE, RULE_NUMER>> parseTable;
+	Map<TOKEN_TYPE, Map<TOKEN_TYPE, RULE_NUMER>> parseTable;
 	
 	
 	List<String> identifiers;
@@ -29,12 +30,6 @@ public class LLParser {
 					FILE_NAMES, SOURCE_FILE, DESTINATION_FILE, EXP_LIST, EXP_LIST_TAIL, EXP, EXP_TAIL, TERM,
 					FILENAME, BIN_OP};
 	
-	
-					enum RULE_TYPE{MINIRE, STATEMENT_LIST, STATEMENT_LIST_TAIL, STATEMENT, ID_STATEMENT,
-		FILE_NAMES, SOURCE_FILE, DESTINATION_FILE, EXP_LIST, EXP_LIST_TAIL, EXP, EXP_TAIL, TERM,
-		FILENAME, BIN_OP};
-
-					
 					
 	enum RULE_NUMER{MINIRE, STATEMENT_LIST, STATEMENT_LIST_TAIL1, STATEMENT_LIST_TAIL2, STATEMENT1, ID_STATEMENT1,
 					ID_STATEMENT2, ID_STATEMENT3, STATEMENT2, STATEMENT3, FILE_NAMES, SOURCE_FILE,
@@ -42,6 +37,20 @@ public class LLParser {
 					EXP_TAIL2, TERM, FILENAME, BIN_OP1, BIN_OP2, BIN_OP3};
 
 	
+	TOKEN_TYPE[] nonTermArray = {TOKEN_TYPE.MINIRE, TOKEN_TYPE.STATEMENT_LIST, TOKEN_TYPE.STATEMENT_LIST_TAIL, 
+			TOKEN_TYPE.STATEMENT, TOKEN_TYPE.ID_STATEMENT, TOKEN_TYPE.FILE_NAMES, TOKEN_TYPE.SOURCE_FILE, 
+			TOKEN_TYPE.DESTINATION_FILE, TOKEN_TYPE.EXP_LIST, TOKEN_TYPE.EXP_LIST_TAIL, TOKEN_TYPE.EXP, TOKEN_TYPE.EXP_TAIL, 
+			TOKEN_TYPE.TERM, TOKEN_TYPE.FILENAME, TOKEN_TYPE.BIN_OP};
+					
+	TOKEN_TYPE[] termArray = {TOKEN_TYPE.ID, TOKEN_TYPE.REPLACE, TOKEN_TYPE.RECURSIVEREPLACE, TOKEN_TYPE.REGEX,
+			TOKEN_TYPE.WITH, TOKEN_TYPE.ASCII_STR, TOKEN_TYPE.IN, TOKEN_TYPE.EMPTY, TOKEN_TYPE.MAXFREQSTRING,
+			TOKEN_TYPE.LEFT_PAREN, TOKEN_TYPE.RIGHT_PAREN, TOKEN_TYPE.HASH_TAG, TOKEN_TYPE.EXC_MARK, TOKEN_TYPE.DIFF, 
+			TOKEN_TYPE.UNION, TOKEN_TYPE.INTERS, TOKEN_TYPE.FIND, TOKEN_TYPE.PRINT, TOKEN_TYPE.COMMA, 
+			TOKEN_TYPE.EQUAL_SIGN, TOKEN_TYPE.SEMI_COLON, TOKEN_TYPE.BEGIN, TOKEN_TYPE.END};
+	
+	
+	List<TOKEN_TYPE> nonTermTokens = Arrays.asList(nonTermArray);
+	List<TOKEN_TYPE> termTokens = Arrays.asList(termArray);
 	
 	public LLParser(List<String> ids, List<String>regex, Stack<String> input){
 		identifiers = ids; 
@@ -50,40 +59,58 @@ public class LLParser {
 		parseStack = new Stack<TOKEN_TYPE>();
 		parseStack.push(TOKEN_TYPE.MINIRE);
 		generateLLTable();
-		
 	}
 	
 	private void generateLLTable(){
 		initializeGrammarRule();
 		
+		parseTable = new HashMap<LLParser.TOKEN_TYPE, Map<TOKEN_TYPE,RULE_NUMER>>();
 		
 		
-	
+		//MINIRE Row
+		TOKEN_TYPE currTokenType = TOKEN_TYPE.MINIRE;
+		Map<TOKEN_TYPE, RULE_NUMER> tableRow= new HashMap<LLParser.TOKEN_TYPE, LLParser.RULE_NUMER>();
+		tableRow.put(TOKEN_TYPE.BEGIN, RULE_NUMER.MINIRE);
+		parseTable.put(currTokenType, tableRow);
+
+		//Statment-List Row
+		currTokenType = TOKEN_TYPE.STATEMENT_LIST;
+		tableRow = new HashMap<LLParser.TOKEN_TYPE, LLParser.RULE_NUMER>();
+		tableRow.put(TOKEN_TYPE.ID, RULE_NUMER.STATEMENT_LIST);
+		tableRow.put(TOKEN_TYPE.REPLACE, RULE_NUMER.STATEMENT_LIST);
+		tableRow.put(TOKEN_TYPE.RECURSIVEREPLACE, RULE_NUMER.STATEMENT_LIST);
+		tableRow.put(TOKEN_TYPE.PRINT, RULE_NUMER.STATEMENT_LIST);
+		parseTable.put(currTokenType, tableRow);
+		
+		
+		//Statment-List-Tail Row
+		currTokenType = TOKEN_TYPE.STATEMENT_LIST;
+		tableRow = new HashMap<LLParser.TOKEN_TYPE, LLParser.RULE_NUMER>();
+		tableRow.put(TOKEN_TYPE.ID, RULE_NUMER.STATEMENT_LIST_TAIL1);
+		tableRow.put(TOKEN_TYPE.REPLACE, RULE_NUMER.STATEMENT_LIST_TAIL1);
+		tableRow.put(TOKEN_TYPE.RECURSIVEREPLACE, RULE_NUMER.STATEMENT_LIST_TAIL1);
+		tableRow.put(TOKEN_TYPE.PRINT, RULE_NUMER.STATEMENT_LIST_TAIL1);
+		tableRow.put(TOKEN_TYPE.EMPTY, RULE_NUMER.STATEMENT_LIST_TAIL2);
+		parseTable.put(currTokenType, tableRow);
+		
+		//Statment Row
+		currTokenType = TOKEN_TYPE.STATEMENT_LIST;
+		tableRow = new HashMap<LLParser.TOKEN_TYPE, LLParser.RULE_NUMER>();
+		tableRow.put(TOKEN_TYPE.ID, RULE_NUMER.STATEMENT1);
+		tableRow.put(TOKEN_TYPE.REPLACE, RULE_NUMER.STATEMENT2);
+		tableRow.put(TOKEN_TYPE.RECURSIVEREPLACE, RULE_NUMER.STATEMENT3);
+		tableRow.put(TOKEN_TYPE.PRINT, RULE_NUMER.STATEMENT4);
+		parseTable.put(currTokenType, tableRow);
+		
+		
+		
+		
 	}
 	
 	
 	
 	private void initializeGrammarRule(){
 	grammarDef = new HashMap<RULE_NUMER, List<TOKEN_TYPE>>();
-	
-//		//MINIRE 
-//		List<TOKEN_TYPE> tempRules = new ArrayList<TOKEN_TYPE>();
-//		tempRules.add(TOKEN_TYPE.STATEMENT_LIST);
-//		grammarDef.put(RULE_NUMER.MINIRE, tempRules);
-//		//statementList
-//		tempRules = new ArrayList<TOKEN_TYPE>();
-//		tempRules.add(TOKEN_TYPE.STATEMENT);
-//		tempRules.add(TOKEN_TYPE.STATEMENT_LIST_TAIL);
-//		grammarDef.put(RULE_NUMER.STATEMENT_LIST, tempRules);
-//		//statement-list-tail1
-//		tempRules = new ArrayList<TOKEN_TYPE>();
-//		tempRules.add(TOKEN_TYPE.STATEMENT);
-//		tempRules.add(TOKEN_TYPE.STATEMENT_LIST_TAIL);
-//		grammarDef.put(RULE_NUMER.STATEMENT_LIST_TAIL1, tempRules);
-//		//statement-list-tail2
-//		tempRules = new ArrayList<TOKEN_TYPE>();
-//		tempRules.add(TOKEN_TYPE.EMPTY);
-//		grammarDef.put(RULE_NUMER.STATEMENT_LIST_TAIL2, tempRules);
 		
 		try{
 		
@@ -195,7 +222,6 @@ private TOKEN_TYPE getTokenType(String token){
 		else if(token.equals("end")){
 			return TOKEN_TYPE.END;
 		}
-		
 		
 		else{
 			return TOKEN_TYPE.ASCII_STR;
