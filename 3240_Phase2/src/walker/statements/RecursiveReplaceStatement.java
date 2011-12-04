ae7c17a28a21b3e74ef6fc463616a6616d3521f2
tkelly910 <tkelly910@gmail.com>
2011-12-04 00:37:58 +0000

    ReplaceStatement and RecursiveReplaceStatement

--- 3240_Phase2/src/walker/statements/RecursiveReplaceStatement.java
+++ 3240_Phase2/src/walker/statements/RecursiveReplaceStatement.java
@@ -1,6 +1,39 @@
-
 package walker.statements;
 
+import java.io.File;
+import walker.exceptions.StatementExecutionException;
+
 public class RecursiveReplaceStatement extends ReplaceStatement {
-    
+    @Override
+    protected boolean replace(String regex, String string, File fromFile, File toFile) throws StatementExecutionException {
+        if (regex.equals(string)) {
+            // Taylor TODO - better error
+            throw new StatementExecutionException("Recursive Replace: Regex cannot equal string");
+        }
+        File tempFile_0 = new File(toFile.getParent(), toFile.getName() + "_tmp0");
+        File tempFile_1 = new File(toFile.getParent(), toFile.getName() + "_tmp1");
+
+        boolean changed = super.replace(regex, string, fromFile, tempFile_0);
+        boolean changedAtAll = changed;
+        boolean fileSwitch = true;
+
+        while (changed) {
+            if (fileSwitch) {
+                changed = super.replace(regex, string, tempFile_0, tempFile_1);
+            } else {
+                changed = super.replace(regex, string, tempFile_1, tempFile_0);
+            }
+            fileSwitch = !fileSwitch;
+        }
+        
+        if(fileSwitch) {
+            tempFile_0.renameTo(toFile);
+            tempFile_1.deleteOnExit();
+        } else {
+            tempFile_1.renameTo(toFile);
+            tempFile_0.deleteOnExit();
+        }
+
+        return changedAtAll;
+    }
 }
--- 3240_Phase2/src/walker/statements/ReplaceStatement.java
+++ 3240_Phase2/src/walker/statements/ReplaceStatement.java
@@ -1,15 +1,73 @@
-
 package walker.statements;
 
 import ast.StatementNode;
+import java.io.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import walker.ExpressionDelegate;
-
+import walker.exceptions.StatementExecutionException;
 
 public class ReplaceStatement implements StatementExecutor {
-
     @Override
-    public void execute(StatementNode node, ExpressionDelegate delegate) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void execute(StatementNode node, ExpressionDelegate delegate) throws StatementExecutionException {
+        if (!(node.value() instanceof String[])) {
+            // Taylor TODO - Better error
+            throw new StatementExecutionException("Incorrect values for replace statement");
+        }
+
+        String[] values = (String[]) node.value();
+
+        if (values.length != 4) {
+            // Taylor TODO - Better error
+            throw new StatementExecutionException("Replace statement requires 4 arguments");
+        }
+
+
+        String fileName1 = values[2];
+        String fileName2 = values[3];
+
+        if (fileName1.contains(fileName2)) {
+            // Taylor TODO - Better error
+            throw new StatementExecutionException("File1 and File2 cannot be the same file");
+        }
+
+        File file1 = new File(fileName1);
+        File file2 = new File(fileName2);
+
+        if (!file1.exists())
+            throw new StatementExecutionException("File1 doesn't exist"); //Taylor TODO - better
+
+
+        replace(values[0], values[1], file1, file2);
     }
-    
+
+    protected boolean replace(String regex, String string, File fromFile, File toFile) throws StatementExecutionException {
+        FileWriter fwriter = null;
+        BufferedWriter bwriter = null;
+        PrintWriter pwriter = null;
+        boolean changed = false;
+        try {
+            fwriter = new FileWriter(toFile);
+            bwriter = new BufferedWriter(fwriter);
+            pwriter = new PrintWriter(pwriter);
+
+            Scanner scan = new Scanner(fromFile);
+
+            while (scan.hasNextLine()) {
+                String str = scan.nextLine();
+                String newStr = str.replaceAll(regex, string);
+                if (!str.equals(newStr)) changed = true;
+                pwriter.println(newStr);
+            }
+            pwriter.flush();
+        } catch (FileNotFoundException ex) {
+            throw new StatementExecutionException("File1 doesn't exist"); //Taylor TODO - better
+        } catch (IOException ex) {
+            throw new StatementExecutionException("Unable to write to file2"); //Taylor TODO - better
+        } finally {
+            pwriter.close();
+            return changed;
+        }
+    }
 }
