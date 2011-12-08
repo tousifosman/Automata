1df93c6c559f201d3a7635b2f737a364f5bfe686
tkelly910@gmail.com <tkelly910@gmail.com@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-12-08 01:05:12 +0000

    Testing Bug Fixes

--- 3240_Phase2/src/walker/ASTWalkerTest.java
+++ 3240_Phase2/src/walker/ASTWalkerTest.java
@@ -7,60 +7,73 @@ import walker.statements.*;
 
 public class ASTWalkerTest {
     public static void main(String[] args) {
-        StatementNode head = new StatementNode(AssignStatement.type(), "a");
-        ExpressionNode binop = new ExpressionNode(BinopExpression.type(), "intersect");
-        ExpressionNode term = new ExpressionNode(TermExpression.type(), new String[]{"[A-Z a-z]*ment[A-Z a-z]*", "file1.txt"});
 
-        ExpressionNode binop2 = new ExpressionNode(BinopExpression.type(), null);
-        ExpressionNode term2 = new ExpressionNode(TermExpression.type(), new String[]{"(A|a) [A-Z a-z]*", "file2.txt"});
-
+        StatementNode head = new StatementNode(AssignStatement.type(), "match_the");
+        ExpressionNode binop = new ExpressionNode(BinopExpression.type(), null);
+        ExpressionNode term = new ExpressionNode(TermExpression.type(), new String[]{"[A-Z a-z]*the[A-Z a-z]*", "input.txt"});
         binop.addSubnode(term);
-        binop.addSubnode(binop2);
-        binop2.addSubnode(term2);
-
         head.addSubnode(binop);
 
+        StatementNode second = new StatementNode(AssignStatement.type(), "match_th");
+        ExpressionNode binop2 = new ExpressionNode(BinopExpression.type(), null);
+        ExpressionNode term2 = new ExpressionNode(TermExpression.type(), new String[]{"th[A-Z a-z]*", "input.txt"});
+        binop2.addSubnode(term2);
+        second.addSubnode(binop2);
 
-        StatementNode second = new StatementNode(AssignStatement.type(), "b");
-        ExpressionNode binop3 = new ExpressionNode(BinopExpression.type(), null);
-        ExpressionNode term3 = new ExpressionNode(TermExpression.type(), new String[]{"[A-Z a-z]*ment[A-Z a-z]*", "file1.txt"});
-        binop3.addSubnode(term3);
-
-        second.addSubnode(term3);
         head.setNextNode(second);
 
-        StatementNode third = new StatementNode(AssignStatement.type(), "c");
+        StatementNode third = new StatementNode(AssignStatement.type(), "count");
+        ExpressionNode the_size = new ExpressionNode(SizeExpression.type(), null);
+        ExpressionNode the_list = new ExpressionNode(IdExpression.type(), "match_the");
+        third.addSubnode(the_size);
+        the_size.addSubnode(the_list);
+
+        second.setNextNode(third);
+
+        StatementNode fourth = new StatementNode(PrintStatement.type(), null);
+        ExpressionNode count = new ExpressionNode(IdExpression.type(), "count");
+        fourth.addSubnode(count);
+
+        third.setNextNode(fourth);
+
+        StatementNode fifth = new StatementNode(AssignStatement.type(), "count");
+        ExpressionNode th_size = new ExpressionNode(SizeExpression.type(), null);
+        ExpressionNode th_list = new ExpressionNode(IdExpression.type(), "match_th");
+        fifth.addSubnode(th_size);
+        th_size.addSubnode(th_list);
+
+        fourth.setNextNode(fifth);
+
+        StatementNode sixth = new StatementNode(PrintStatement.type(), null);
+        sixth.addSubnode(count);
+        sixth.addSubnode(th_list);
+
+        fifth.setNextNode(sixth);
+
+        StatementNode seventh = new StatementNode(PrintStatement.type(), null);
+        ExpressionNode binop3 = new ExpressionNode(BinopExpression.type(), "union");
+        ExpressionNode term3 = new ExpressionNode(TermExpression.type(), new String[]{"[A-Z a-z]*z[A-Z a-z]*", "input.txt"});
         ExpressionNode binop4 = new ExpressionNode(BinopExpression.type(), null);
-        ExpressionNode term4 = new ExpressionNode(TermExpression.type(), new String[]{"(A|a) [A-Z a-z]*", "file2.txt"});
+        ExpressionNode term4 = new ExpressionNode(TermExpression.type(), new String[]{"[A-Z a-z]*q[A-Z a-z]*", "input.txt"});
+        seventh.addSubnode(binop3);
+        binop3.addSubnode(term3);
+        binop3.addSubnode(binop4);
         binop4.addSubnode(term4);
 
-        third.addSubnode(term4);
-        second.setNextNode(third);
-        
-        
-        StatementNode print = new StatementNode(PrintStatement.type(), null);
-        ExpressionNode a = new ExpressionNode(IdExpression.type(), "a");
-        ExpressionNode b = new ExpressionNode(IdExpression.type(), "b");
-        ExpressionNode c = new ExpressionNode(IdExpression.type(), "c");
-        ExpressionNode d = new ExpressionNode(SizeExpression.type(), null);
-        d.addSubnode(a);
-        
-        print.addSubnode(a);
-        print.addSubnode(b);
-        print.addSubnode(c);
-        print.addSubnode(d);
-        
-        third.setNextNode(print);
-        
-        StatementNode replace = new StatementNode(ReplaceStatement.type(), new String[]{"[A-Z a-z]*ment", "", "file1.txt", "file3.txt"});
-        print.setNextNode(replace);
+        sixth.setNextNode(seventh);
+
+        StatementNode eighth = new StatementNode(ReplaceStatement.type(), new String[]{"we|We|they|They", "I", "input.txt", "result1.txt"});
+        seventh.setNextNode(eighth);
+
+        StatementNode ninth = new StatementNode(ReplaceStatement.type(), new String[]{"h[A-z a-z]*", "anana", "input.txt", "result2.txt"});
+        eighth.setNextNode(ninth);
 
         AbstractSyntaxTree tree = new AbstractSyntaxTree(head);
         ASTWalker walker = new ASTWalker(System.out);
         try {
             walker.walk(tree);
         } catch (ASTExecutionException ex) {
-            System.out.println("error");
+            ex.printStackTrace();
         }
     }
 }
