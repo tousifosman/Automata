9967e8ebf6350d6f8ee6d6f336d6b27469528073
tkelly910@gmail.com <tkelly910@gmail.com@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-12-10 04:14:40 +0000

    Merging Scanner and Main

--- 3240_Phase2/src/scanner/ScriptScanner.java
+++ 3240_Phase2/src/scanner/ScriptScanner.java
@@ -1,8 +1,6 @@
 package scanner;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
@@ -18,7 +16,6 @@ import scanner.exceptions.SyntaxErrorException;
 import scanner.generateNFA.FinalNFA;
 
 public class ScriptScanner {
-
     public static List<String> identifiers;
     public static List<String> regexes;
     public static List<String> strconsts;
@@ -63,9 +60,59 @@ public class ScriptScanner {
         }
     }
 
+    /**
+     * Creates the DFA and ScannerDriver using the files containing the regexes
+     * and the tokens.
+     * 
+     * If no arguments are supplied, the user is prompted for inputs
+     * 
+     * @param args 1st argument is used as the name of the file containing all 
+     * of the tokens
+     * @throws SyntaxErrorException 
+     * @throws IOException 
+     * @throws FileNotFoundException 
+     */
+    public static void scan(String input) throws FileNotFoundException, IOException, SyntaxErrorException {
+        identifiers = new ArrayList<String>();
+        regexes = new ArrayList<String>();
+        strconsts = new ArrayList<String>();
+        tokens = new ArrayDeque<String>();
+
+        //System.out.println("Using Lexical Specification for MinRE: " + ScriptScanner.class.getResource("MiniRE_LexSpec.txt").toString().substring(6));
+        //DFA dfa = generateDFA(ScriptScanner.class.getResource("MiniRE_LexSpec.txt").toString().substring(6));
+
+        DFA dfa = generateDFA("MiniRE_LexSpec.txt");
+
+        File temp_file = new File("temp.tmp");
+        FileWriter f = null;
+        BufferedWriter b = null;
+        PrintWriter p = null;
+
+        try {
+            f = new FileWriter(temp_file, true);
+            b = new BufferedWriter(f);
+            p = new PrintWriter(b);
+
+            p.print(input);
+        } catch (IOException e) {
+            System.out.println("Error creating temp file");
+        } finally {
+            if (p != null) p.close();
+        }
+
+
+        try {
+            ScannerDriver driver = new ScannerDriver(temp_file, dfa);
+            driver.run();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error: Cannot find file");
+            System.out.println(ex.getMessage());
+        }
+    }
+
     private static DFA generateDFA(String fileName) throws FileNotFoundException, IOException, SyntaxErrorException {
         FinalNFA NFAgen = new FinalNFA();
-        NFA nfa  = NFAgen.generate(fileName);
+        NFA nfa = NFAgen.generate(fileName);
         return NFAtoDFA.dfaFromNFA(nfa);
     }
 }
