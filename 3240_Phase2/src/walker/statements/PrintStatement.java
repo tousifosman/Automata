ef534b1497ea765133b5fbe80b066e4998ef1eb1
tkelly910@gmail.com <tkelly910@gmail.com@30609bc4-1d05-7d63-98a3-feb33e2c2c53>
2011-12-10 03:46:31 +0000

    Adjusted Walker for Parser output

--- 3240_Phase2/src/walker/statements/PrintStatement.java
+++ 3240_Phase2/src/walker/statements/PrintStatement.java
@@ -8,6 +8,7 @@ import walker.ExpressionDelegate;
 import main.PrintStream;
 import walker.exceptions.ASTExecutionException;
 import walker.exceptions.IncorrectNodeTypeException;
+import walker.exceptions.StatementArgumentException;
 import walker.exceptions.StatementExecutionException;
 
 public class PrintStatement implements StatementExecutor {
@@ -20,15 +21,37 @@ public class PrintStatement implements StatementExecutor {
     @Override
     public void execute(StatementNode node, ExpressionDelegate delegate) throws ASTExecutionException {
         List<Node> subnodes = node.subnodes();
+        if (subnodes.size() == 1 && subnodes.get(0) instanceof ExpressionNode && subnodes.get(0).type().equals("EXP_LIST")) {
+            subnodes = subnodes.get(0).subnodes();
+            if (subnodes.size() == 2 && subnodes.get(0) instanceof ExpressionNode && subnodes.get(1) instanceof ExpressionNode) {
+                Object result = delegate.expand((ExpressionNode) subnodes.get(1), null);
+                out.println(result);
+                ExpressionNode currNode = (ExpressionNode) subnodes.get(0);
 
-        for (Node subnode : subnodes) {
-            if (!(subnode instanceof ExpressionNode)) {
-                throw new IncorrectNodeTypeException(this.getClass().getSimpleName() + " Error: Requires ExpressionNodes", subnode);
+                while (currNode.type().equals("EXP_LIST_TAIL")) {
+                    subnodes = currNode.subnodes();
+
+                    if (subnodes.size() == 2 && subnodes.get(0) instanceof ExpressionNode && subnodes.get(1) instanceof ExpressionNode) {
+                        result = delegate.expand((ExpressionNode) subnodes.get(1), null);
+                        out.println(result);
+                        currNode = (ExpressionNode) subnodes.get(0);
+                    } else {
+                        throw new IncorrectNodeTypeException(this.getClass().getSimpleName() + " Error: Requires 2 ExpressionNodes", null);
+                    }
+
+                }
+                if (!currNode.type().equals("EXP_LIST_TAIL1")) {
+                    throw new StatementArgumentException(this.getClass().getSimpleName() + " Error: Requires ExpListTails as left nodes");
+                }
+            } else {
+                throw new IncorrectNodeTypeException(this.getClass().getSimpleName() + " Error: EXP_LIST Requires 2 ExpressionNodes", null);
             }
-            
-            Object result = delegate.expand((ExpressionNode)subnode, null);
-            out.println(result);
+        } else {
+            throw new StatementArgumentException(this.getClass().getSimpleName() + " Error: Requires 1 EXP_LIST, but got " + subnodes.get(0).type());
+
         }
+
+
     }
 
     public static String type() {
